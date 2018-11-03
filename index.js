cc428231c9a3f5f0fc0a881fd305f48489a7974c
kinokus <aleks.kinokus@gmail.com>
2018-11-03 12:49:17 +0200

    some training

--- index.js
+++ index.js
@@ -11,13 +11,15 @@ jQuery(document).ready(function ($) {
     $('#message-to-send').val('');
   };
   
-  let checkInput = (event)=>{
-    if(event.which === 13 && $('#message-to-send').val().length) {
+  let checkInput = (event) => {
+    if (event.which === 13 && $('#message-to-send').val().length) {
       sendMessage();
     }
   };
   
-  
+  // $('.msg-string').load(function () {
+  //   console.log(this);
+  // })
   
   socket.on('connect', function () {
     let myId = (socket.id).toString().substr(0, 5);
@@ -71,14 +73,30 @@ jQuery(document).ready(function ($) {
         }
       }
       if (msg.add) {
-        $('#messages-got-from-server > .messages-list')
-          .append(`
-                            <span class="msg-string">
+        let msgLine = $(`
+                            <span class="msg-string" style="display: none">
                             <pre class="msg-time">[${msg.time}]</pre>
                             <span class="${msg.userClass}">${msg.nickFrom || msg.name}</span>
                             <span class="${msg.class}">${msg.text}</span>
                             </span>
                         `);
+        $('#messages-got-from-server > .messages-list').append(msgLine);
+        msgLine.hover(
+          function () {
+            // $(this).css({border: '0px solid green'});
+            $(this).animate({height: '+=10px'}, 'fast');
+            $(this).animate({fontSize: '+=10px'}, 'fast');
+            
+            // $(this).animate({borderWidth: '0px'});
+            
+          },
+          function () {
+            $(this).animate({height: '-=10px'}, 'fast');
+            $(this).animate({fontSize: '-=10px'}, 'fast');
+  
+          });
+        msgLine.show(1000);
+        
       }
       if (msg.status) {
         $('#status-got-from-server')
@@ -91,12 +109,84 @@ jQuery(document).ready(function ($) {
                         `)
       }
       
-      if(msg.nick){
-      
-      }
-      
-      
     });
+    
+    $('#nick-current')
+      .focus(function () {
+        $(this).css({background: 'wheat'})
+      })
+      .blur(function () {
+        $(this).css({background: 'none'})
+      })
+      .select(function () {
+        let value = $(this).val();
+        let start = $(this)[0].selectionStart;
+        let end = $(this)[0].selectionEnd;
+        let selectedValue = value.substr(start, end - start);
+        
+        console.log(selectedValue);
+        
+      })
+    
+    $('#nick-current').change(function () {
+    })
+    
+    
+    $('body')
+      .on(
+        {
+          'click': function () {
+            console.log(this);
+            // $(this).slideToggle()
+            // $(this).fadeTo(1000,0.3);
+            // $(this).off('click');
+            // $(this).trigger('my-event', ['a1', 'a2', 'a3']);
+            $(this).css({border: '0px solid green'});
+            $(this).animate({borderWidth: '+=5px', borderColor: 'red'}, 3000);
+            $(this).animate({borderWidth: '0px'});
+            
+          },
+          'my-event': function () {
+            console.log('some event ', arguments[0], arguments[1], arguments[2]);
+          }/*,
+          'mouseEnter': function (event) {
+            $(this).css({border:'0px solid green'});
+            $(this).animate({borderWidth:'+=5px',borderColor:'red'}, 3000);
+            $(this).animate({borderWidth:'0px'});
+  
+            // console.log($(event.relatedTarget));
+          }*/
+        },
+        '.msg-string'
+      );
+    
+    $("#send-message-to-server").hover(function (event) {
+      console.log($(event.relatedTarget));
+      $(event.relatedTarget).focus()
+    });
+    $("#form-send-to-server").hover(function (event) {
+      $(event.relatedTarget).focus()
+    });
+    
+    
+    $('.messages-list').resize(function () {
+      console.log(this);
+    })
+    
+    
+    // .select(function () {
+    //   console.log($(this));
+    // })
+    //
+    
+    $('#nick-current').parent()
+      .focusin(function () {
+        $(this).css({border: '1px solid blue'})
+      })
+      .focusout(function () {
+        $(this).css({border: '0px'})
+      });
+    
     
   });
   
